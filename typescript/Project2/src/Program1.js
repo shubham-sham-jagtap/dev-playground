c3a9d723efd11a1a05d02dd5ea09f64fe2699a3f
Shubham Jagtap <shubham.jagtap@indexnine.com>
2026-03-13 00:03:48 -0500

    narrowing concept

--- typescript/Project2/src/Program1.js
+++ typescript/Project2/src/Program1.js
@@ -4,4 +4,46 @@ function getSomething(something) {
     }
     return "we are returning a number i.e., ".concat(something);
 }
-console.log(getSomething(24));
+function fun1(msg) {
+    if (msg) {
+        return "Got the message, the message is: ".concat(msg);
+    }
+    return "No message";
+}
+var Demo1 = /** @class */ (function () {
+    function Demo1() {
+    }
+    Demo1.prototype.fun = function () {
+        return "Demo1 fun";
+    };
+    return Demo1;
+}());
+var Demo2 = /** @class */ (function () {
+    function Demo2() {
+    }
+    Demo2.prototype.fun = function () {
+        return "Demo1 fun";
+    };
+    return Demo2;
+}());
+function fun(blueprint) {
+    if (blueprint instanceof Demo1) {
+        console.log(blueprint.fun());
+    }
+    return console.log(blueprint.fun());
+}
+function isChaiOrder(obj) {
+    return (typeof obj === "object" &&
+        typeof obj.type === "string" &&
+        typeof obj.name === "number" &&
+        obj != null);
+}
+function serveChai(item) {
+    if (isChaiOrder(item)) {
+        return "the object type is chai order";
+    }
+    return "the received data is ".concat(item);
+}
+// console.log(getSomething(24));
+// console.log(fun());
+console.log(serveChai("Shubham"));
--- typescript/Project2/src/Program1.ts
+++ typescript/Project2/src/Program1.ts
@@ -5,4 +5,50 @@ function getSomething(something: number | string): string {
     return `we are returning a number i.e., ${something}`
 }
 
-console.log(getSomething(24));
+function fun1(msg?: string) {
+    if(msg) {
+        return `Got the message, the message is: ${msg}`;
+    }
+    return `No message`;
+}
+
+class Demo1 {
+    fun() {
+        return `Demo1 fun`;
+    }
+}
+
+class Demo2 {
+    fun() {
+        return `Demo1 fun`;
+    }
+}
+
+function fun(blueprint: Demo1 | Demo2): any {
+    if(blueprint instanceof Demo1) {
+        console.log(blueprint.fun());
+    }
+    return console.log(blueprint.fun());
+}
+
+type chaiOrder = {
+    type: string
+    sugar: number
+}
+
+function isChaiOrder(obj: any): obj is chaiOrder {
+    return (typeof obj === "object" && 
+        typeof obj.type === "string" && 
+        typeof obj.name === "number" &&
+    obj != null);
+}
+
+function serveChai(item: string | chaiOrder) {
+    if (isChaiOrder(item)) {
+        return `the object type is chai order`;
+    }
+    return `the received data is ${item}`;
+}
+// console.log(getSomething(24));
+// console.log(fun());
+console.log(serveChai("Shubham"))
